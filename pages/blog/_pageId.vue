16b5b9e88121df20610d5b189d4c01f87e21217b
shunta nakajima <nakg_asimo@icloud.com>
2021-09-06 23:44:37 +0900

    fix ogp

--- pages/blog/_pageId.vue
+++ pages/blog/_pageId.vue
@@ -40,8 +40,8 @@ export default class BlogContent extends Vue {
       return {
         title: this.pageItem?.Title.title[0].plain_text,
         meta: [
-          { hid: 'og:url', property: 'og:url', content: `https/shunta.dev/blog/${this.page_id}` },
-          { hid: 'og:image', property: 'og:image', content: `https/shunta.dev/ogp/${this.page_id}.png` },
+          { hid: 'og:url', property: 'og:url', content: `https://shunta.dev/blog/${this.page_id}` },
+          { hid: 'og:image', property: 'og:image', content: `https://shunta.dev/ogp/${this.page_id}.png` },
           { hid: 'og:title', property: 'og:title', content: this.pageItem?.Title.title[0].plain_text },
           { hid: 'og:type', property: 'og:type', content: "article" },
         ]
